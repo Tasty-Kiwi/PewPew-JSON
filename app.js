e578f77ba474f7da67412f6119adc9736203b257
Dominykas M <aabcde689@gmail.com>
2021-07-11 21:55:01 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+/*
+ * My shitty pewpewjson compiler
+ * (c) Tasty Kiwi, 2021
+ * Licensed under MIT license
+ */
+
+function compile() {
+	// some prep code 
+	const raw_code = document.getElementById("codearea").value 
+	let temp_arr = ["-- this file was created by pewpewjson compiler", "pewpew.print(\"This level was created using PewPewJson: https://github.com/Tasty-Kiwi/PewPewJson\")\n"]
+
+	try {
+		// convert json to js object
+		const parsed_code = JSON.parse(raw_code)
+
+		// convert objects into lua code
+		if (parsed_code.options.level_size) {temp_arr.push(`pewpew.set_level_size(${parsed_code.options.level_size[0]}fx, ${parsed_code.options.level_size[1]}fx)`)}
+		if (parsed_code.options.player_location) {temp_arr.push(`local ship = pewpew.new_player_ship(${parsed_code.options.player_location[0]}fx, ${parsed_code.options.player_location[1]}fx, 0)`)}
+		if (parsed_code.options.shield_amount >= 0) {temp_arr.push(`pewpew.configure_player(0, {shield = ${parsed_code.options.shield_amount}})`)}
+
+		// join the array of code 
+		document.getElementById("codeoutput").innerHTML = temp_arr.join('\n')
+	}
+	catch (error){
+		// send error if some shit happened
+		document.getElementById("codeoutput").innerHTML = `-- ERROR: ${error}`
+	}
+}
