ae074f358b30ea8b90217d705d26919636337d26
Dominykas M <aabcde689@gmail.com>
2021-10-12 16:19:03 +0300

    Probably last update of 1.0 era

--- app.js
+++ app.js
@@ -3,17 +3,19 @@
  * Copyright (c) 2021 Dominykas M. (Tasty Kiwi)
  * Licensed under MIT license
  */
- "use strict";
- function compile() {
- 	const raw_code = document.getElementById("codearea").value
+
+function compile() {
+	const raw_code = document.getElementById("codearea").value
 	// compile code
-	try { 
+	try {
 		let temp_arr = ["-- this level file was created by pewpewjson compiler\n"]
 		// if no text, throw an exception
-		if (!raw_code) {throw "Did you forget to put something in?"}
-			
+		if (!raw_code) {
+			throw "Error: Did you forget to put something in?"
+		}
+
 		// convert json to js object
-	const parsed_code = JSON.parse(raw_code)
+		const parsed_code = JSON.parse(raw_code)
 
 		// convert objects into lua code
 		if (parsed_code.options.use_helpers) {
@@ -48,77 +50,91 @@
 
 		//this oddball ensures the game ends
 		temp_arr.push(`pewpew.add_update_callback(function() if pewpew.get_player_configuration(0)["has_lost"] == true then pewpew.stop_game() end end)`)
-		let entity_array = parsed_code.entities
-		for (var i = 0; i < entity_array.length; i++) {
-			if (entity_array[i].entity_type.toUpperCase() == "CUSTOMIZABLE_ENTITY") {
-				temp_arr.push(`pewpew.customizable_entity_set_mesh(pewpew.new_customizable_entity(${entity_array[i].attributes[0]}fx, ${entity_array[i].attributes[1]}fx), "/dynamic/${entity_array[i].mesh_path}", 0)`)
+		if (parsed_code.entities) {
+			for (let i = 0; i < parsed_code.entities.length; i++) {
+				if (parsed_code.entities[i].entity_type.toUpperCase() == "CUSTOMIZABLE_ENTITY") {
+					temp_arr.push(`pewpew.customizable_entity_set_mesh(pewpew.new_customizable_entity(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx), "/dynamic/${parsed_code.entities[i].mesh_path}", 0)`)
+				}
 			}
 		}
 		if (parsed_code.options.respawn_enemies) {
 			temp_arr.push(`pewpew.add_update_callback(function() if pewpew.get_entity_count(pewpew.EntityType.MOTHERSHIP) + pewpew.get_entity_count(pewpew.EntityType.ASTEROID) + pewpew.get_entity_count(pewpew.EntityType.BAF) + pewpew.get_entity_count(pewpew.EntityType.INERTIAC) + pewpew.get_entity_count(pewpew.EntityType.ROLLING_CUBE) + pewpew.get_entity_count(pewpew.EntityType.WARY) + pewpew.get_entity_count(pewpew.EntityType.UFO) + pewpew.get_entity_count(pewpew.EntityType.CROWDER) == 0 then`)
 		}
 		//enemy code
-		for (var i = 0; i < entity_array.length; i++) {
-			if (entity_array[i].entity_type.toUpperCase() == "WARY") {
-				temp_arr.push(`pewpew.new_wary(${entity_array[i].attributes[0]}fx, ${entity_array[i].attributes[1]}fx)`)
-			}
-			if (entity_array[i].entity_type.toUpperCase() == "ASTEROID") {
-				temp_arr.push(`pewpew.new_asteroid(${entity_array[i].attributes[0]}fx, ${entity_array[i].attributes[1]}fx)`)
-			}
-			if (entity_array[i].entity_type.toUpperCase() == "CROWDER") {
-				temp_arr.push(`pewpew.new_crowder(${entity_array[i].attributes[0]}fx, ${entity_array[i].attributes[1]}fx)`)
-			}
-			if (entity_array[i].entity_type.toUpperCase() == "ROLLING_CUBE") {
-				if (entity_array[i].wall_collisions) {
-					temp_arr.push(`pewpew.rolling_cube_set_enable_collisions_with_walls(pewpew.new_rolling_cube(${entity_array[i].attributes[0]}fx, ${entity_array[i].attributes[1]}fx), true)`)
-				} else {
-					temp_arr.push(`pewpew.new_rolling_cube(${entity_array[i].attributes[0]}fx, ${entity_array[i].attributes[1]}fx)`)
+		if (parsed_code.entities) {
+			for (let i = 0; i < parsed_code.entities.length; i++) {
+				if (parsed_code.entities[i].entity_type.toUpperCase() == "WARY") {
+					temp_arr.push(`pewpew.new_wary(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx)`)
+				}
+				if (parsed_code.entities[i].entity_type.toUpperCase() == "ASTEROID") {
+					temp_arr.push(`pewpew.new_asteroid(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx)`)
+				}
+				if (parsed_code.entities[i].entity_type.toUpperCase() == "CROWDER") {
+					temp_arr.push(`pewpew.new_crowder(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx)`)
+				}
+				if (parsed_code.entities[i].entity_type.toUpperCase() == "ROLLING_CUBE") {
+					if (parsed_code.entities[i].wall_collisions) {
+						temp_arr.push(`pewpew.rolling_cube_set_enable_collisions_with_walls(pewpew.new_rolling_cube(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx), true)`)
+					} else {
+						temp_arr.push(`pewpew.new_rolling_cube(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx)`)
+					}
+				}
+				if (parsed_code.entities[i].entity_type.toUpperCase() == "UFO") {
+					if (parsed_code.entities[i].wall_collisions) {
+						temp_arr.push(`pewpew.ufo_set_enable_collisions_with_walls(pewpew.new_ufo(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx, ${parsed_code.entities[i].attributes[2]}fx), true)`)
+					} else {
+						temp_arr.push(`pewpew.new_ufo(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx, ${parsed_code.entities[i].attributes[2]}fx)`)
+					}
+				}
+				if (parsed_code.entities[i].entity_type.toUpperCase() == "BOMB") {
+					temp_arr.push(`pewpew.new_bomb(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx, pewpew.BombType.${parsed_code.entities[i].bomb_type.toUpperCase()})`)
+				}
+				if (parsed_code.entities[i].entity_type.toUpperCase() == "INERTIAC") {
+					temp_arr.push(`pewpew.new_inertiac(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx, ${parsed_code.entities[i].attributes[2]}fx, ${parsed_code.entities[i].attributes[3]}fx)`)
+				}
+				if (parsed_code.entities[i].entity_type.toUpperCase() == "ROLLING_SPHERE") {
+					temp_arr.push(`pewpew.new_rolling_sphere(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx, ${parsed_code.entities[i].attributes[2]}fx, ${parsed_code.entities[i].attributes[3]}fx)`)
+				}
+				if (parsed_code.entities[i].entity_type.toUpperCase() == "BAF") {
+					temp_arr.push(`pewpew.new_baf(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx, ${parsed_code.entities[i].attributes[2]}fx, ${parsed_code.entities[i].attributes[3]}fx, ${parsed_code.entities[i].attributes[4]})`)
+				}
+				if (parsed_code.entities[i].entity_type.toUpperCase() == "BAF_BLUE") {
+					temp_arr.push(`pewpew.new_baf_blue(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx, ${parsed_code.entities[i].attributes[2]}fx, ${parsed_code.entities[i].attributes[3]}fx, ${parsed_code.entities[i].attributes[4]})`)
+				}
+				if (parsed_code.entities[i].entity_type.toUpperCase() == "BAF_RED") {
+					temp_arr.push(`pewpew.new_baf_red(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx, ${parsed_code.entities[i].attributes[2]}fx, ${parsed_code.entities[i].attributes[3]}fx, ${parsed_code.entities[i].attributes[4]})`)
+				}
+				if (parsed_code.entities[i].entity_type.toUpperCase() == "MOTHERSHIP") {
+					temp_arr.push(`pewpew.new_mothership(${parsed_code.entities[i].attributes[0]}fx, ${parsed_code.entities[i].attributes[1]}fx, pewpew.MothershipType.${parsed_code.entities[i].mothership_type.toUpperCase()}, ${parsed_code.entities[i].attributes[2]}fx)`)
 				}
 			}
-			if (entity_array[i].entity_type.toUpperCase() == "UFO") {
-				if (entity_array[i].wall_collisions) {
-					temp_arr.push(`pewpew.ufo_set_enable_collisions_with_walls(pewpew.new_ufo(${entity_array[i].attributes[0]}fx, ${entity_array[i].attributes[1]}fx, ${entity_array[i].attributes[2]}fx), true)`)
-				} else {
-					temp_arr.push(`pewpew.new_ufo(${entity_array[i].attributes[0]}fx, ${entity_array[i].attributes[1]}fx, ${entity_array[i].attributes[2]}fx)`)
-				}
-			}
-			if (entity_array[i].entity_type.toUpperCase() == "BOMB") {
-				temp_arr.push(`pewpew.new_bomb(${entity_array[i].attributes[0]}fx, ${entity_array[i].attributes[1]}fx, pewpew.BombType.${entity_array[i].bomb_type.toUpperCase()})`)
-			}
-			if (entity_array[i].entity_type.toUpperCase() == "INERTIAC") {
-				temp_arr.push(`pewpew.new_inertiac(${entity_array[i].attributes[0]}fx, ${entity_array[i].attributes[1]}fx, ${entity_array[i].attributes[2]}fx, ${entity_array[i].attributes[3]}fx)`)
-			}
-			if (entity_array[i].entity_type.toUpperCase() == "ROLLING_SPHERE") {
-				temp_arr.push(`pewpew.new_rolling_sphere(${entity_array[i].attributes[0]}fx, ${entity_array[i].attributes[1]}fx, ${entity_array[i].attributes[2]}fx, ${entity_array[i].attributes[3]}fx)`)
-			}
-			if (entity_array[i].entity_type.toUpperCase() == "BAF") {
-				temp_arr.push(`pewpew.new_baf(${entity_array[i].attributes[0]}fx, ${entity_array[i].attributes[1]}fx, ${entity_array[i].attributes[2]}fx, ${entity_array[i].attributes[3]}fx, ${entity_array[i].attributes[4]})`)
-			}
-			if (entity_array[i].entity_type.toUpperCase() == "MOTHERSHIP") {
-				temp_arr.push(`pewpew.new_mothership(${entity_array[i].attributes[0]}fx, ${entity_array[i].attributes[1]}fx, pewpew.MothershipType.${entity_array[i].mothership_type.toUpperCase()}, ${entity_array[i].attributes[2]}fx)`)
-			}
-
 		}
+
 		if (parsed_code.options.respawn_enemies) {
-			temp_arr.push(`end`,`end)`)
+			temp_arr.push(`end`, `end)`)
 		}
 
-		if (parsed_code.helper_extras.use_shield_box && parsed_code.options.use_helpers && parsed_code.helper_extras.modulo_max) {
-			temp_arr.push(`local time1 = 0`, `pewpew.add_update_callback(function()`, 'time1 = time1 + 1', `local modulo1 = time1 % ${parsed_code.helper_extras.modulo_max}`, `if modulo1 == ${parsed_code.helper_extras.shield_box_modulo} then shield_box.new(fmath.random_fixedpoint(10fx, ${parsed_code.options.level_size[0]}fx - 10fx), fmath.random_fixedpoint(10fx, ${parsed_code.options.level_size[1]}fx - 10fx), weapon_config)`, `end`, `end)`)
+
+		if (parsed_code.options.use_helpers){
+			if (parsed_code.helper_extras.use_shield_box && parsed_code.helper_extras.modulo_max) {
+				temp_arr.push(`local time1 = 0`, `pewpew.add_update_callback(function()`, 'time1 = time1 + 1', `local modulo1 = time1 % ${parsed_code.helper_extras.modulo_max}`, `if modulo1 == ${parsed_code.helper_extras.shield_box_modulo} then shield_box.new(fmath.random_fixedpoint(10fx, ${parsed_code.options.level_size[0]}fx - 10fx), fmath.random_fixedpoint(10fx, ${parsed_code.options.level_size[1]}fx - 10fx), weapon_config)`, `end`, `end)`)
+			}
+	
+			if (parsed_code.helper_extras.use_cannon_box && parsed_code.helper_extras.modulo_max) {
+				temp_arr.push(`local time2 = 0`, `pewpew.add_update_callback(function()`, 'time2 = time2 + 1', `local modulo2 = time2 % ${parsed_code.helper_extras.modulo_max}`, `if modulo2 == ${parsed_code.helper_extras.cannon_box_modulo} then cannon_box.new(fmath.random_fixedpoint(10fx, ${parsed_code.options.level_size[0]}fx - 10fx), fmath.random_fixedpoint(10fx, ${parsed_code.options.level_size[1]}fx - 10fx), fmath.random_int(0, 4))`, `end`, `end)`)
+			}
+	
 		}
 
-		if (parsed_code.helper_extras.use_cannon_box && parsed_code.options.use_helpers && parsed_code.helper_extras.modulo_max) {
-			temp_arr.push(`local time2 = 0`, `pewpew.add_update_callback(function()`, 'time2 = time2 + 1', `local modulo2 = time2 % ${parsed_code.helper_extras.modulo_max}`, `if modulo2 == ${parsed_code.helper_extras.cannon_box_modulo} then cannon_box.new(fmath.random_fixedpoint(10fx, ${parsed_code.options.level_size[0]}fx - 10fx), fmath.random_fixedpoint(10fx, ${parsed_code.options.level_size[1]}fx - 10fx), fmath.random_int(0, 4))`, `end`, `end)`)
-		}
+
 		// join the array of code 
 		document.getElementById("codeoutput").innerHTML = temp_arr.join('\n')
-	}
-	catch (error){
+	} catch (error) {
 		// send error if some shit happened
-		document.getElementById("codeoutput").innerHTML = `-- ERROR: ${error}`
+		document.getElementById("codeoutput").innerHTML = `-- ${error}`
 	}
 	//mesh generator
-	try{
+	try {
 		let mesh_arr = []
 		if (JSON.parse(raw_code).options.level_size) {
 			if (JSON.parse(raw_code).mesh_options.mesh_color) {
@@ -127,11 +143,10 @@
 				mesh_arr = ["-- this mesh was file created by pewpewjson compiler\n", `meshes = {{vertexes = {{0,0}, {0,${JSON.parse(raw_code).options.level_size[1]}}, {${JSON.parse(raw_code).options.level_size[0]},${JSON.parse(raw_code).options.level_size[1]}}, {${JSON.parse(raw_code).options.level_size[0]},0}}, segments = {{0,1,2,3,0}}, colors = {0xffffffff, 0xffffffff, 0x$ffffffff, 0xffffffff}}}`]
 			}
 		} else {
-			throw "level_size is missing."
+			throw "Error: level_size is missing."
 		}
 		document.getElementById("meshoutput").innerHTML = mesh_arr.join('\n')
-	}
-	catch (error){
-		document.getElementById("meshoutput").innerHTML = `-- ERROR: ${error}`
+	} catch (error) {
+		document.getElementById("meshoutput").innerHTML = `-- ${error}`
 	}
 }
